53cc1ba71e50da008c1ce9491107462cfa2ec618
Ryan Fletcher <fletcher.ryj@gmail.com>
2026-03-10 15:03:01 +0200

    coded the encoder

---
+++ encoder.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+map<char,string> get_encoding(string scheme);
+string encode(string, map<char, string>);
+
+int main()
+{
+    map<char, string> encoding;
+
+    string scheme;
+    getline(cin, scheme);
+
+    encoding = get_encoding(scheme);
+
+    string line;
+    getline(cin, line);
+
+    cout << encode(line, encoding) << endl;
+
+    return 0;
+}
+
+/**
+ * @brief Retireves encoding of symbols.
+ * @param scheme line describing the encodign scheme used.
+ * @return Map containing <symbol, encoding>
+ */
+map<char, string> get_encoding(string scheme) {
+    map<char, string> encoding;
+    stringstream ss(scheme);
+    string pair;
+
+    while (getline(ss, pair, '|')) {
+        int pos = pair.find('=');
+
+        char symbol = pair[0];
+        string code = pair.substr(pos+1);
+
+        encoding[symbol] =  code;
+    }
+
+    return encoding;
+}
+
+/**
+ * @brief Encodes a string given an encoding scheme.
+ * @param line line to be encoded.
+ * @param encoding the encoding scheme to be used.
+ * @return Encoded line.
+ */
+string encode(string line, map<char,string> encoding) {
+    string output = "";
+    for (char c : line) {
+        output += encoding[c];
+    }
+    return output;
+}
